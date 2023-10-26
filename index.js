76377e8b9f15fa1623c590c1ef148065215ea139
desarrollo52022 <desarrollo5.corporacionphara@outlook.com>
2023-10-26 08:12:00 -0600

    Cambio el puerto

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const routes = require('./src/routes/route');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000; 
+const port = process.env.PORT || 3000;
 
 
 // Middleware para procesar JSON
